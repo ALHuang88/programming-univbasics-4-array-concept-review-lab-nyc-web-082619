aca29f6e40357dabaa8ce2d995e8afc583702d66
Allen Huang <alhuang1288@gmail.com>
2019-08-23 15:59:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,9 @@ def find_element_index(array, value_to_find)
     puts nil
 end
 
-def find_max_value(array)
-  
+def find_max_value([1,2,1,3,4,3,5,4,3,2,1])
+  while find_min_value([1,2,1,3,4,3,5,4,3,2,1]) do
+    
 end
 
 def find_min_value(array)
